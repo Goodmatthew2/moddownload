f0c60ad2af5fdad279c5349a01a285956a7cb838
Goodmatthew2 <46863140+Goodmatthew2@users.noreply.github.com>
2023-04-20 20:40:46 +0800

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,7 +1,22 @@
 [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null 
 $modsFolder = "$env:APPDATA\.minecraft\mods" 
 $url = "https://dl.dropbox.com/s/7kd2qkzqs24dj6b/mods.zip?dl=0"
+$clienturl = "https://dl.dropbox.com/s/e6wrsstppj69wie/client.zip?dl=0"
 $tempFile = "$env:TEMP\minecraftmods.zip" 
+
+function hi {
+	$choice = Read-Host "Download ClientMods? (Y/N)"
+	if ($choice -eq "Y") {
+		Yes
+	} elseif ($choice -eq "N") {
+		No
+	} else {
+		Write-Host "Invalid choice. Please enter Y or N."
+		hi
+	}
+}
+
+hi
 
 Write-Host @"
                                                 __                                    
@@ -24,14 +39,32 @@ Write-Host @"
                                                                                       
 "@ 
 
-Remove-Item -Path "$env:APPDATA\.minecraft\mods\*" -Force -Recurse 
-Write-Host "Download will take a while" -ForegroundColor Red -BackgroundColor White
-Write-Host "Downloading Mods" -ForegroundColor Green -BackgroundColor White
-Write-Host "Update April 12 2023" -ForegroundColor Blue -BackgroundColor White
-Invoke-WebRequest -Uri $url -OutFile $tempFile -UseBasicParsing
-Write-Host "Extracting Mods" -ForegroundColor Blue -BackgroundColor Black
-Expand-Archive -Path $tempFile -DestinationPath $modsFolder -Force 
-Remove-Item $tempFile 
-(New-Object Media.SoundPlayer "C:\Windows\Media\Windows Background.wav").Play()
-Write-Host "Server Version: 1.19.3 Forge" -ForegroundColor Green -BackgroundColor White
-[System.Windows.Forms.MessageBox]::Show("Finished Downloading Mods")
+function Yes {
+	Remove-Item -Path "$env:APPDATA\.minecraft\mods\*" -Force -Recurse 
+	Write-Host "Download will take a while" -ForegroundColor Red -BackgroundColor White
+	Write-Host "Downloading Mods" -ForegroundColor Green -BackgroundColor White
+	Write-Host "Update April 12 2023" -ForegroundColor Blue -BackgroundColor White
+	Invoke-WebRequest -Uri $url -OutFile $tempFile -UseBasicParsing
+	Write-Host "Extracting Mods" -ForegroundColor Blue -BackgroundColor Black
+	Expand-Archive -Path $tempFile -DestinationPath $modsFolder -Force 
+	Remove-Item $tempFile 
+	(New-Object Media.SoundPlayer "C:\Windows\Media\Windows Background.wav").Play()
+	Write-Host "Server Version: 1.19.3 Forge" -ForegroundColor Green -BackgroundColor White
+	[System.Windows.Forms.MessageBox]::Show("Finished Downloading Mods")
+	exit
+}
+
+function No {
+	Remove-Item -Path "$env:APPDATA\.minecraft\mods\*" -Force -Recurse 
+	Write-Host "Download will take a while" -ForegroundColor Red -BackgroundColor White
+	Write-Host "Downloading Mods" -ForegroundColor Green -BackgroundColor White
+	Write-Host "Update April 20 2023" -ForegroundColor Blue -BackgroundColor White
+	Invoke-WebRequest -Uri $clienturl -OutFile $tempFile -UseBasicParsing
+	Write-Host "Extracting Mods" -ForegroundColor Blue -BackgroundColor Black
+	Expand-Archive -Path $tempFile -DestinationPath $modsFolder -Force 
+	Remove-Item $tempFile 
+	(New-Object Media.SoundPlayer "C:\Windows\Media\Windows Background.wav").Play()
+	Write-Host "Server Version: 1.19.4 Fabric/Vanilla/Paper" -ForegroundColor Green -BackgroundColor White
+	[System.Windows.Forms.MessageBox]::Show("Finished Downloading Mods")
+	exit
+}
